4353d7d8281106bf140967b79ed0089710b69a7c
Siddarth Dagar <sidmcheath@gmail.com>
2020-12-30 17:57:43 -0500

    Completing pages

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 $(window).on('load', function () {
     make_border_style('#box', '+=+| |+=+', '#928374');
+    make_border_style('#cite', '#-#| |#-#', '#928374')
 });
 
 function make_border_style(selector, text, color, $parent) {
